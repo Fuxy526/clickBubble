bc556d3b9b2ff11d7248b67ec2b14fe9feb8e925
Fuxy526 <261867120@qq.com>
2016-04-11 18:06:07 +0800

    first edition

---
+++ js/jquery.clickBubble.js
@@ -0,0 +1,42 @@
+(function($) {
+
+	$.fn.clickBubble = function(options) {
+
+		var defaultOptions = {
+			color: '#000',
+			size: 20,
+			time: 500,
+			borderWidth: '2px'
+		}
+
+		var id = 0;
+
+		options = $.extend(defaultOptions, options);
+
+		$(this).on("mousedown", function(e) {
+			var x = e.clientX;
+			var y = $(window).scrollTop() + e.clientY;
+			$('<div>').attr('id', 'clickCircle_' + id++).css({
+				'width': 0,
+				'height': 0,
+				'border': defaultOptions.borderWidth + ' solid ' + options.color,
+				'position': 'absolute',
+				'left': x,
+				'top': y,
+				'border-radius': '50%'
+			}).animate({
+				'width': defaultOptions.size + 'px',
+				'height': defaultOptions.size + 'px',
+				'left': x - 0.5 * defaultOptions.size,
+				'top': y - 0.5 * defaultOptions.size,
+				opacity: 0
+			}, defaultOptions.time, function() {
+				$(this).remove();
+			}).appendTo('body');
+		});
+
+		return this;
+
+	}
+
+})(jQuery);
